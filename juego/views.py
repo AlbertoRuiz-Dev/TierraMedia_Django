4d5dd4c83f303e3da8566612708438a54ac75df5
jperrob <jperrob244@fp.iesromerovargas.com>
2025-02-03 12:28:21 +0100

    Cambio de IndexView.

--- juego/views.py
+++ juego/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 
 # Create your views here.
 
-class IndexView(View):
+class IndexView(TemplateView):
     template_name = 'juego/index.html'
-    def get(self, request):
-        return render(request, self.template_name)
