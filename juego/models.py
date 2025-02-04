d41b2a9fb720e38aa64572ef374e66bc2dd95f16
agutcan <agutcan1709@fp.iesromerovargas.com>
2025-02-04 12:42:55 +0100

    Pruebas con modelos.

--- juego/models.py
+++ juego/models.py
@@ -3,29 +3,19 @@ from django.db import models
 # Create your models here.
 #Branch subida
 
-class Weapon(models.Model):
-    name = models.CharField(max_length=50)
-    type = models.CharField(max_length=50)
-    description = models.TextField(max_length=100)
-    damage = models.IntegerField(default=0)
-
-
-class Armor(models.Model):
-    name = models.CharField(max_length=50)
-    type = models.CharField(max_length=50)
-    description = models.TextField(max_length=100)
-    defense = models.IntegerField(default=0)
-
-
 class Equipment(models.Model):
     name = models.CharField(max_length=50)
-    weapon = models.OneToOneField(Weapon, on_delete=models.CASCADE, primary_key=True, blank=True)
-    armor = models.OneToOneField(Armor, on_delete=models.CASCADE, primary_key=True, blank=True)
     description = models.TextField(max_length=100)
+    type = models.CharField(max_length=50)
 
+class Weapon(Equipment):
+    damage = models.IntegerField(default=0)
+
+class Armor(Equipment):
+    defense = models.IntegerField(default=0)
 
 class Character(models.Model):
     name = models.CharField(max_length=50)
-    inventory = [models.ManyToManyField(Equipment)]
-    armor_equipped = models.ManyToManyField(Armor)
-    weapon_equipped = models.ManyToManyField(Weapon)
+    inventory = models.ManyToManyField(Equipment, blank=True, related_name="characters_with_item")
+    armor_equipped = models.ForeignKey(Armor, on_delete=models.SET_NULL, null=True, blank=True, related_name="characters_equipped_armor")
+    weapon_equipped = models.ForeignKey(Weapon, on_delete=models.SET_NULL, null=True, blank=True, related_name="characters_equipped_weapon")
--- juego/tests/test_models.py
+++ juego/tests/test_models.py
@@ -11,12 +11,21 @@ class TestModels(TestCase):
         self.equipamiento_arma = self.arma
         self.equipamiento_armadura = self.armadura
 
-        self.personaje = Character.objects.create(name="Rodolfo")
-        self.personaje.weapon_equipped.add(self.arma)
-        self.personaje.armor_equipped.add(self.armadura)
-        self.personaje.inventory.append(self.equipamiento_arma)
-        self.personaje.inventory.append(self.equipamiento_armadura)
-        self.personaje.save()
+        self.personaje = Character.objects.create(name="Rodolfo", weapon_equipped=self.arma, armor_equipped=self.armadura)
+        self.personaje.inventory.add(self.equipamiento_arma)
+        self.personaje.inventory.add(self.equipamiento_armadura)
+
 
     def test_character(self):
+        self.assertEqual(self.personaje.inventory.count(), 2)
+        self.assertIn(self.arma.id, self.personaje.inventory.values_list('id', flat=True))
+        self.assertIn(self.armadura.id, self.personaje.inventory.values_list('id', flat=True))
+
+        self.assertIn(self.personaje, self.arma.characters_equipped_weapon.all())
+        self.assertIn(self.personaje, self.armadura.characters_equipped_armor.all())
+
+        self.assertIn(self.personaje, self.arma.characters_with_item.all())
+        self.assertIn(self.personaje, self.armadura.characters_with_item.all())
+
         self.assertEqual(self.personaje.weapon_equipped.name, "Daga")
+        self.assertEqual(self.personaje.armor_equipped.name, "Armadura")
