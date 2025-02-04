01427f1646ada5674c20c5cad8015118492444b7
ajimper3009 <ajimper3009@fp.iesromerovargas.com>
2025-02-04 19:09:11 +0100

    Añado las urls, views (todavia por hacer) y los modelos (a completar)

--- juego/urls.py
+++ juego/urls.py
@@ -4,4 +4,33 @@ from juego import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('chararcter/characters', views.ListView.as_view(), name='playerListView'),
+    path('faction', views.FactionCharacterListView.as_view(), name='factionPlayersListView'),
+    path('character/equipment', views.EquipmentCharacterListView.as_view(), name='equipmentPlayersListView'),
+    path('relation', views.RelationCreateView.as_view(), name='relationCreateView'),
+    path('character_create', views.CharacterCreateView.as_view(), name='playerCreateView'),
+    path('character/weapon', views.WeaponCreateView.as_view(), name='weaponCreateView'),
+    path('battle', views.BattleView.as_view(), name='battleCreateView'),
+    path('character/location', views.LocationUpdateView.as_view(), name='locationUpdateView'),
+    path('character/inventory', views.InventoryUpdateView.as_view(), name='inventoryUpdateView'),
+    path('character/equipment_update', views.EquipmentUpdateView.as_view(), name='equipmentUpdateView'),
 ]
+
+"""
+    
+    Pagina principal donde se muestra un menu con las difrentes funciones
+    Pagina para el login/logout
+    Pagina para mostrar los personajes de una faccion
+    Pagina para mostrar los personajes segun un equipamiento
+    Pagina para las batallas
+    
+    Opcion Personaje:
+        Pagina para crear el personaje
+        Pagina para crear las relaciones
+        Pagina para mostrar todos los personajes
+        Pagina para modificar inventario
+        Pagina para modificar equipamiento
+        Pagina para cambiar la localizacion
+        Pagina para crear las armas
+    
+"""
--- juego/views.py
+++ juego/views.py
@@ -1,10 +1,41 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView
 
 
 # Create your views here.
 
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'juego/index.html'
+
+class CharacterListView(LoginRequiredMixin, ListView):
+    pass
+
+class FactionCharacterListView(LoginRequiredMixin, ListView):
+    pass
+
+class EquipmentCharacterListView(LoginRequiredMixin, ListView):
+    pass
+
+class RelationCreateView(LoginRequiredMixin, CreateView):
+    pass
+
+class CharacterCreateView(LoginRequiredMixin, CreateView):
+    pass
+
+class BattleView(LoginRequiredMixin, TemplateView):
+    pass
+
+class WeaponCreateView(LoginRequiredMixin, CreateView):
+    pass
+
+class LocationUpdateView(LoginRequiredMixin, UpdateView):
+    pass
+
+class InventoryUpdateView(LoginRequiredMixin, UpdateView):
+    pass
+
+class EquipmentUpdateView(LoginRequiredMixin, UpdateView):
+    pass
