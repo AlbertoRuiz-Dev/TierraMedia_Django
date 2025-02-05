6e5f88f32e98cc07e45f41ece9f716837f8aac80
aaron <agutcan1709@f.iesromerovargas.es>
2025-02-05 12:22:36 +0100

    Views y archivo character_list.html modificado.

--- juego/views.py
+++ juego/views.py
@@ -43,6 +43,10 @@ class CharacterListView(ListView):
     template_name = 'juego/character_list.html'
     context_object_name = 'character_list'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
 class FactionCharacterListView(LoginRequiredMixin, ListView):
     model = Faction
     template_name = ''
