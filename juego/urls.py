f7071ba7b5ce4f5d752a0273fa5e83a41554fedc
Jesus <jperrob244@fp.iesromerovargas.com>
2025-02-06 13:00:15 +0100

    Login hecho y vistas base añadidas.

--- juego/urls.py
+++ juego/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from juego import views
 
+app_name = 'juego'
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('personaje/', views.PersonajeView.as_view(), name='personaje'),
+    path('equipamiento/', views.EquipamientoView.as_view(), name='equipamiento'),
+    path('faccion/', views.FaccionView.as_view(), name='faccion'),
+    path('batalla/', views.BatallaView.as_view(), name='batalla'),
 ]
