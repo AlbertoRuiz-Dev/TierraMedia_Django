5c003dbfda3e2030c92c5859d68d9def2a8a8484
agutcan <agutcan1709@fp.iesromerovargas.com>
2025-02-11 09:55:45 +0100

    Actualización de test views.

--- juego/tests/test_views.py
+++ juego/tests/test_views.py
@@ -136,7 +136,7 @@ class FactionCharacterFormViewTest(TestCase):
     def test_faction_form(self):
         """Verifica que un personaje sin inventario ni facción muestra 'Vacio' y 'No hay inventario'"""
         self.client.login(username='testuser', password='password123')
-        response = self.client.get(self.character_list_url)
+        response = self.client.post(self.faction_character_form_url {})
 
         self.assertContains(response, "Gimli")  # Otro personaje sin facción ni inventario
         self.assertContains(response, "Vacio")  # No tiene facción
