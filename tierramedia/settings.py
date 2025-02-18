0d4d3813e69ad0510eba6ffb2965802f2a474fba
agutcan <agutcan1709@fp.iesromerovargas.com>
2025-02-18 09:51:54 +0100

    Actualización de rama.

--- tierramedia/settings.py
+++ tierramedia/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'juego.apps.JuegoConfig',
+    "debug_toolbar",
+
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'tierramedia.urls'
@@ -81,6 +84,7 @@ DATABASES = {
     }
 }
 
+INTERNAL_IPS = ['127.0.0.1']
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
