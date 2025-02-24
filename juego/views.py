9fdb7a8c9575e2dc8c2c5b5c77dea89e9b0f6db4
agutcan <agutcan1709@fp.iesromerovargas.com>
2025-02-24 12:08:48 +0100

    Actualización de rama.

--- juego/views.py
+++ juego/views.py
@@ -85,8 +85,8 @@ class EquipmentCharacterFormView(LoginRequiredMixin, FormView):
     def form_valid(self, form):
         weapon = form.cleaned_data["weapon"]  # Obtiene el arma seleccionada
         armor = form.cleaned_data["armor"]  # Obtiene la armadura seleccionada
-        characters = Character.objects.select_related('equipped_weapon', 'equipped_armor').prefetch_related(
-            'inventory__weapons', 'inventory__armors'  # Relaciones de muchos a muchos
+        characters = Character.objects.select_related('equipped_weapon', 'equipped_armor', 'faction').prefetch_related(
+            'inventory_weapons', 'inventory_armors'  # Relaciones de muchos a muchos
         ).all()
 
         if weapon or armor:
